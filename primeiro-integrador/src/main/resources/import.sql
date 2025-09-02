e9daa229316343dee2d1f62067353691f167a9f5
aliviaprado977 <160698412+aliviaprado977@users.noreply.github.com>
2025-09-02 01:44:13 +0000

    Aula 01/09

---
+++ primeiro-integrador/src/main/resources/import.sql
@@ -0,0 +1,43 @@
+insert into Categoria (nome) values ('Carnes');
+insert into Categoria (nome) values ('Bebidas');
+insert into Categoria (nome) values ('Congelados');
+insert into Categoria (nome) values ('Bolo');
+insert into Categoria (nome) values ('Massas');
+insert into Categoria (nome) values ('Limpeza');
+insert into Categoria (nome) values ('Chocolate');
+insert into Categoria (nome) values ('Higiene');
+insert into Categoria (nome) values ('Maionese');
+insert into Categoria (nome) values ('Perfumaria');
+
+
+insert into Fabricante (nome) values ('JBS');
+insert into Fabricante (nome) values ('The Coca Cola Company');
+insert into Fabricante (nome) values ('Seara');
+insert into Fabricante (nome) values ('Panco');
+insert into Fabricante (nome) values ('Barilla');
+insert into Fabricante (nome) values ('Downy');
+insert into Fabricante (nome) values ('Lacta');
+insert into Fabricante (nome) values ('Francis');
+insert into Fabricante (nome) values ('Heinz');
+insert into Fabricante (nome) values ('Dove');
+
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Picanha', 2, 55.50, '2025-09-20', 1, 2);
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Coca Cola 2 Litros', 20, 12.50, '2025-12-20', 2, 3);
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Lasanha Congelada', 10, 18.90, '2026-01-15', 3, 4);
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Bolo de Chocolate', 15, 9.99, '2025-10-10', 4, 5);
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Macarrão Espaguete', 30, 6.45, '2026-02-28', 5, 6);
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Amaciante Downy 1L', 25, 11.30, '2026-03-05', 6, 7);
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Barra de Chocolate Ao Leite', 40, 5.99, '2026-05-10', 7, 8);
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Sabonete Francis', 50, 2.99, '2026-06-20', 8, 9);
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Maionese Heinz 500g', 35, 8.49, '2025-11-30', 9, 10);
+
+insert into Produto (nome, quantidade, preco, validade, categoria_codigo, fabricante_codigo) values ('Desodorante Dove Aerosol', 45, 14.90, '2026-04-15', 10, 11);
